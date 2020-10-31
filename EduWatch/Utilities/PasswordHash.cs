aebd7f48568ca6b88b08349fee25d652e3c32d55
Todor Pakov <trpakov@abv.bg>
2020-10-31 16:29:56 +0200

    Moved disposable objects into using statements

--- EduWatch/Utilities/PasswordHash.cs
+++ EduWatch/Utilities/PasswordHash.cs
@@ -19,14 +19,16 @@ namespace EduWatch.Utilities
             rng.GetBytes(salt);
 
             // Create hash array and generate hash using Rfc2898DeriveBytes, which implements password-based key derivation
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations);
-            byte[] hash = pbkdf2.GetBytes(HashSize);
+            using(var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations))
+            {
+                byte[] hash = pbkdf2.GetBytes(HashSize);
 
-            // Combine salt and hash and return the result as a base-64 string
-            byte[] saltAndHash = new byte[SaltSize + HashSize];
-            Array.Copy(salt, 0, saltAndHash, 0, SaltSize);
-            Array.Copy(hash, 0, saltAndHash, SaltSize, HashSize);
-            return Convert.ToBase64String(saltAndHash);
+                // Combine salt and hash and return the result as a base-64 string
+                byte[] saltAndHash = new byte[SaltSize + HashSize];
+                Array.Copy(salt, 0, saltAndHash, 0, SaltSize);
+                Array.Copy(hash, 0, saltAndHash, SaltSize, HashSize);
+                return Convert.ToBase64String(saltAndHash);
+            }
         }
 
         internal static bool Verify(string userInputPass, string saltAndHashString)
@@ -37,12 +39,14 @@ namespace EduWatch.Utilities
             Array.Copy(saltAndHash, 0, salt, 0, SaltSize);
 
             // Use the password entered by the user and the salt to generate a hash
-            var pbkdf2 = new Rfc2898DeriveBytes(userInputPass, salt, Iterations);
-            byte[] hash = pbkdf2.GetBytes(HashSize);
+            using(var pbkdf2 = new Rfc2898DeriveBytes(userInputPass, salt, Iterations))
+            {
+                byte[] hash = pbkdf2.GetBytes(HashSize);
 
-            // If the two hashes are different the password is incorrect, else its correct
-            for (int i = 0; i < HashSize; i++)
-                if (hash[i] != saltAndHash[SaltSize + i]) return false;
+                // If the two hashes are different the password is incorrect, else its correct
+                for (int i = 0; i < HashSize; i++)
+                    if (hash[i] != saltAndHash[SaltSize + i]) return false;
+            }
 
             return true;
         }
