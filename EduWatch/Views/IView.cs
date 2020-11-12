7af676817156dbf535b44dab745c10a7ae8696bc
Todor Pakov <trpakov@abv.bg>
2020-11-12 12:39:36 +0200

    Create IView interface declaring functionality, shared by all views

---
+++ EduWatch/Views/IView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduWatch.Views
+{
+    // Interface to be used with all views
+    public interface IView
+    {
+        void Hide();
+        void Show();
+        void Close();
+        void Message(string msg, string caption = "Съобщение", MessageIcon msgIcon = MessageIcon.None, MessageButton msgBtn = MessageButton.OK);
+    }
+
+    // Universal enum with Message icons
+    public enum MessageIcon
+    {
+        None,
+        Warning,
+        Error,
+        Exclamation,
+        Information
+    }
+
+    // Universal enum with Message buttons
+    public enum MessageButton
+    {
+        OK,
+        OKCancel,
+        YesNo,
+        YesNoCancel
+    }
+}
