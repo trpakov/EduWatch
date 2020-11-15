9b20563eeeba7077aaf4becabe668d954d164ac9
Todor Pakov <trpakov@abv.bg>
2020-11-15 14:26:38 +0200

    Add buttons for saving and canceling the changes made to the seen status of a given grade by the user, addresses #6

--- EduWatch/Views/ParentForm.designer.cs
+++ EduWatch/Views/ParentForm.designer.cs
@@ -41,6 +41,8 @@
             this.radioButtonGrades = new System.Windows.Forms.RadioButton();
             this.radioButtonNotes = new System.Windows.Forms.RadioButton();
             this.LblName = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -151,6 +153,7 @@
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.Size = new System.Drawing.Size(832, 160);
             this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CurrentCellDirtyStateChanged += new System.EventHandler(this.dataGridView1_CurrentCellDirtyStateChanged);
             // 
             // comboBoxStudent
             // 
@@ -212,6 +215,36 @@
             this.LblName.TabIndex = 24;
             this.LblName.Text = "*името на родителя*";
             // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Transparent;
+            this.btnSave.Enabled = false;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSave.Location = new System.Drawing.Point(605, 586);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(126, 29);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "Запази";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.Enabled = false;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancel.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCancel.Location = new System.Drawing.Point(748, 586);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(126, 29);
+            this.btnCancel.TabIndex = 26;
+            this.btnCancel.Text = "Отказ";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
             // ParentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -220,6 +253,8 @@
             this.BackgroundImage = global::EduWatch.Properties.Resources.background1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(968, 659);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.LblName);
             this.Controls.Add(this.radioButtonNotes);
             this.Controls.Add(this.radioButtonGrades);
@@ -258,5 +293,7 @@
         private System.Windows.Forms.RadioButton radioButtonGrades;
         private System.Windows.Forms.RadioButton radioButtonNotes;
         private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
